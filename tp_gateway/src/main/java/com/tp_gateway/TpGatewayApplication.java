aaef5b5d2c171c3f523b183783c475b8ba4718e3
Jackvideo <13226110808@163.com>
2023-12-13 19:48:22 +0800

    完成网关模块路由功能的基本搭建

--- tp_gateway/src/main/java/com/tp_gateway/TpGatewayApplication.java
+++ tp_gateway/src/main/java/com/tp_gateway/TpGatewayApplication.java
@@ -2,8 +2,13 @@ package com.tp_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.tp_gateway"
+})
+@EnableDiscoveryClient
 public class TpGatewayApplication {
 
     public static void main(String[] args) {
--- tp_user/src/main/java/com/tp_user/TpUserApplication.java
+++ tp_user/src/main/java/com/tp_user/TpUserApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(scanBasePackages = {"com.tp_user"})
+@SpringBootApplication(scanBasePackages = {"com.tp_common","com.tp_user"})
 @EnableDiscoveryClient
 @EnableFeignClients
 public class TpUserApplication {
